aff325e2f2f90d5e66f19c09414a6d3942a772d8
Romina Rosa <romina-rosa@outlook.com>
2023-03-18 19:10:20 -0300

    CRUD parte 2-Sitio Cati kids

--- Sitio Proyecto Final/models/promocionesModel.js
+++ Sitio Proyecto Final/models/promocionesModel.js
@@ -1,16 +1,47 @@
 var pool = require("./bd");
 
 /*sirve para listar las promociones*/
-async function getPromociones () {
-        var query = "select * from promociones";
-        var rows = await pool.query(query);
-        return rows;
-    }
-module.exports = {getPromociones}
+async function getPromociones() {
+    var query = "select * from promociones";
+    var rows = await pool.query(query);
+    return rows;
+}
+module.exports = { getPromociones }
 
 async function deletePromocionesById(id) {
     var query = "delete from promociones where  id = ?";
     var rows = await pool.query(query, [id]);
     return rows;
 }
-module.exports = {getPromociones, deletePromocionesById}
+
+//Funcion para dar de alta una nueva promocion//
+async function insertPromociones(obj) {
+    try {
+        var query = "insert into promociones set ?";
+        var rows = await pool.query(query, [obj]);
+        return rows;
+
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }//cierra CATCH
+}//cierra INSERT
+
+//traigo los datos para modificar una SOLA promocion//
+async function getPromocionesById(id) {
+    var query = "select * from promociones where id = ?";
+    var rows = await pool.query(query, [id]);
+    return rows[0];
+}
+
+async function modificarPromocionesById(obj, id) {
+    try {
+        var query = "update promociones set ? where id=? ";
+        var rows = await pool.query(query, [obj, id]);
+        return rows;
+    } catch (error) {
+        throw error;
+    }
+}//cierra modificar update//
+
+module.exports = { getPromociones, deletePromocionesById, insertPromociones, getPromocionesById, modificarPromocionesById }
--- Sitio Proyecto Final/routes/admin/promociones.js
+++ Sitio Proyecto Final/routes/admin/promociones.js
@@ -20,7 +20,67 @@ router.get('/eliminar/:id', async (req, res, next) => {
     const id = req.params.id;
     await promocionesModel.deletePromocionesById(id);
     res.redirect("/admin/promociones")
+}); /*cierra eliminar una promocion*/
+
+/*diseno de la pag agregar una promocion*/
+router.get('/agregar', (req, res, next) => {
+    res.render('admin/agregar', {
+        layout: 'admin/layout'
+    }); //cierra render//
+});//cierra GET//
+
+//inserta Promocion en base de datos//
+router.post('/agregar', async (req, res, next) => {
+    try {
+        if (req.body.titulo != "" && req.body.info != "") {
+            await promocionesModel.insertPromociones(req.body);
+            res.redirect('/admin/promociones')
+
+        } else {
+            res.render("admin/agregar", {
+                layout: 'admin/layout',
+                error: true, message: "Todos los campos son requeridos"
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.render("admin/agregar", {
+            layout: 'admin/layout',
+            error: true, message: "No se ha cargado la promoción"
+        });
+    }
 });
+//diseno de: modificar + traer la novedad que yo seleccione//
+router.get('/modificar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    var promociones = await promocionesModel.getPromocionesById(id);
+    res.render("/admin/modificar", {
+        layout: 'admin/layout',
+        promociones
+    });
+});//cierro get de modificar
+
+//actualizar la promocion//
+router.post('/modificar', async (req, res, next) => {
+    try {
+        console.log (req.body.id); //para ver si trae id
+        var obj = {
+            titulo: req.body.titulo,
+            info: req.body.info,
+        }
+        console.log(obj)//para ver si trae los datos
+        await promocionesModel.modificarPromocionesById(obj, req.body.id);
+        res.redirect("/admin/promociones");
+
+    } catch (error) {
+        console.log(error)
+        res.render("admin/modificar", {
+            layout: 'admin/layout',
+            error: true, 
+            message: "No se ha modificado la promoción"
+        })
+    }//cierro el catch
+});//cierro el post
 
 
 module.exports = router;
